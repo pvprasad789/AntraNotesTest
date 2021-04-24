a625b761b45dda06d45e9b40077cc055267d6447
pvprasad789 <pvpindian@gmail.com>
2021-04-24 12:14:47 +0530

    fixed note updated issue.

--- App/Reducers/NotesReducer.ts
+++ App/Reducers/NotesReducer.ts
@@ -1,34 +1,40 @@
-import {NotesActionTypes} from '../Actions/NotesActions'
+import {NotesActionTypes} from '../Actions/NotesActions';
 
 const initialState = {
   notesArr: [],
-}
+};
 
 const notesReducer = (state = initialState, action: any) => {
-  switch(action.type) {
+  switch (action.type) {
     case NotesActionTypes.ADD_NOTE:
-      const noteId = state.notesArr.length > 0 ? state.notesArr[state.notesArr.length - 1].id + 1 : 1
+      const noteId =
+        state.notesArr.length > 0
+          ? state.notesArr[state.notesArr.length - 1].id + 1
+          : 1;
       return {
-        notesArr: [...state.notesArr, {...action.payload, id: noteId}]
-      }
+        notesArr: [...state.notesArr, {...action.payload, id: noteId}],
+      };
     case NotesActionTypes.UPDATE_NOTE:
-      const temp = state.notesArr
-      temp.map((item: any) => {
-        if (item.id === action.payload.id) {
-          item.title = action.payload.title
-          item.description = action.payload.description
-        }
-      })
       return {
-        notesArr: temp
-      }
+        notesArr: state.notesArr.map((note: any) =>
+          note.id === action.payload.id
+            ? {
+                ...note,
+                title: action.payload.title,
+                description: action.payload.description,
+              }
+            : note,
+        ),
+      };
     case NotesActionTypes.DELETE_NOTE:
       return {
-        notesArr: state.notesArr.filter((item: any) => item.id !== action.noteId)
-      }
+        notesArr: state.notesArr.filter(
+          (item: any) => item.id !== action.noteId,
+        ),
+      };
     default:
-      return state
+      return state;
   }
-}
+};
 
-export default notesReducer
+export default notesReducer;
